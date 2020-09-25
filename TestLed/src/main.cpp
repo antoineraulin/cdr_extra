e8e445c33c61f70a91810b55c80c1ed8f7466bbe
Antoine Raulin <25278917+antoineraulin@users.noreply.github.com>
2020-09-25 09:43:34 +0200

    second commit

--- TestLed/src/main.cpp
+++ TestLed/src/main.cpp
@@ -1,39 +1,3 @@
-// #include <Arduino.h>
-// #include <FastLED.h>
-// #define LED_PIN 5
-// #define NUM_LEDS 14
-// CRGB leds[NUM_LEDS];
-
-// void setup()
-// {
-//   // put your setup code here, to run once:
-//   pinMode(13, OUTPUT);
-
-//   Serial.begin(9600);
-
-//   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-// }
-
-// void loop()
-// {
-//     for (int i = 0; i <= 3; i++)
-//     {
-//       leds[2 + i] = CRGB(255, 0, 0);
-//       leds[6 + i] = CRGB(255, 0, 0);
-//       leds[10 + i] = CRGB(255, 0, 0);
-//       FastLED.show();
-//       delay(50);
-//       leds[2 + i] = CRGB(0, 0, 0);
-//       leds[6 + i] = CRGB(0, 0, 0);
-//       leds[10 + i] = CRGB(0, 0, 0);
-//       FastLED.show();
-//       delay(50);
-//     }
-    
-  
-// }
-
-
 //Déclaration éléments pour les servos :
 #include <Arduino.h>
 #include <Servo.h>
@@ -56,36 +20,31 @@ int val = 0;
 
 void Monter()
 {
-
-  // for (pos = 100; pos <= 190; pos += spe)
-  // {
-  //   myservohaut.write(pos);
-  //   delay(de);
-  // }
-  myservohaut.write(180);
+  myservohaut.write(178);
   myservobas.write(10);
 }
 
 void setup()
 {
 
-  pinMode(7, INPUT); // sets the digital pin 7 as input
-  pinMode(13, OUTPUT);
+  pinMode(7, INPUT);   // pin 7 : Main Button
+  pinMode(13, OUTPUT); // internal led
 
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS); //init led strip
 
-  
+  for (int i = 0; i < 14; i++) //reset all leds
+  {
+    leds[i] = CRGB(0, 0, 0);
+  }
 
-
-  
   myservohaut.attach(10); // Le servo haut sur le pin 9 de l'Arduino
   myservobas.attach(9);   // Le servo bas au pin 10 de l'Arduino
 
   myservohaut.write(100); //on reset la position des bras
   myservobas.write(85);
-  Serial.begin(9600);
-  
-  while(digitalRead(7) == HIGH){
+
+  while (digitalRead(7) == HIGH)
+  { // waiting for button switch, blinking internal led
     digitalWrite(13, HIGH);
     delay(50);
     digitalWrite(13, LOW);
@@ -96,17 +55,11 @@ void setup()
 void loop()
 {
 
-  // val = digitalRead(2);
-  // while (val == HIGH)
-  // {
-  //   val = digitalRead(2);
-  // }
+  Monter(); // monte le bras
 
-  Monter();
-
-  while (true) //val == LOW
+  while (true) //boucle infini pour les leds
   {
-    
+
     for (int i = -1; i <= NUM_LEDS; i++)
     {
       leds[i + 1] = CRGB(0, 0, 255);
@@ -114,10 +67,6 @@ void loop()
       leds[i + 3] = CRGB(255, 0, 0);
       leds[i] = CRGB(0, 0, 0);
 
-      /*leds[i+10] = CRGB ( 0, 0, 255);
-    leds[i+11] = CRGB ( 255, 255, 255);
-    leds[i+12] = CRGB ( 255, 0, 0);
-    leds[i+9] = CRGB ( 0, 0, 0);*/
       FastLED.show();
       delay(60);
     }
@@ -128,13 +77,8 @@ void loop()
       leds[i] = CRGB(255, 0, 0);
       leds[i + 1] = CRGB(0, 0, 0);
 
-      /*leds[i-10] = CRGB ( 255, 0, 0);
-    leds[i-11] = CRGB ( 255, 255, 255);
-    leds[i-12] = CRGB ( 0, 0, 255);
-    leds[i-9] = CRGB ( 0, 0, 0);*/
       FastLED.show();
       delay(60);
     }
   }
 }
-
