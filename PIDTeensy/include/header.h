d4e2bda931add8f6ce923fb2ae9a4a2e20f4420d
COLVERTYETY <niconoob1@gmail.com>
2020-09-25 17:55:44 +0200

    debugging interrupts

--- PIDTeensy/include/header.h
+++ PIDTeensy/include/header.h
@@ -1,9 +1,16 @@
+#include <stdint.h>
+
 //CYCLE
 void Cycle();
-const int period = 100000; //10 Hz
+const int period = 100000; //10 Hz 1 000 000 microseconds = 1 second
 
 //PID - variables
 volatile int tick = 0; //encoder ticks
 
 //Communication
 const int bauderate = 9600;
+
+
+//encoder
+const int encoderpin = 8;// pin for the encoder interrupt
+void encoderInterrupt();//isr for encoder
--- PIDTeensy/src/main.cpp
+++ PIDTeensy/src/main.cpp
@@ -2,22 +2,36 @@
 #include <Arduino.h>
 #include "header.h"
 
+const int led=LED_BUILTIN;//used for debug
 void setup(void)
 {
+  //TIMER initialization
+  pinMode(led,OUTPUT); //used for debug
   Timer1.initialize(period); //initialisation du timer
   Timer1.attachInterrupt(Cycle); //attachInterrupt
+  //encoder initialisation
+  attachInterrupt(encoderpin,encoderInterrupt,RISING);
   Serial.begin(bauderate);
+
+}
+
+void loop(void)
+{
+  delay(300);
+  Serial.println("a delay has ended");
+  delay(500);
+  Serial.println("other");
+  digitalWrite(led,LOW);
 }
 
 void Cycle()
 {
   cli(); //éteint les interrupts
-  tick++;
   Serial.println(tick);
   sei(); //relance les interrupts
 }
 
-void loop(void)
-{
-  delay(100);
+void encoderInterrupt(){
+  tick++;
+  digitalWrite(led,HIGH);
 }
