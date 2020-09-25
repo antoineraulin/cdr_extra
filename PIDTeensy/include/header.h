99f5aff8896d8364732e892fef41a24aa3ea5dab
COLVERTYETY <niconoob1@gmail.com>
2020-09-25 17:09:01 +0200

    changed for pid

--- PIDTeensy/include/header.h
+++ PIDTeensy/include/header.h
@@ -2,15 +2,34 @@
 
 //CYCLE
 void Cycle();
-const int period = 100000; //10 Hz 1 000 000 microseconds = 1 second
+const int period = 100000; //10 Hz ; 1 000 000 microseconds = 1 second
+
+//ros comm
+volatile int target_ticks=0;//target number of ticks per cycle
+volatile int target_duration=0;//number of cycles to run
+volatile bool emergency_break=false;//boolean for emergeancy break
 
 //PID - variables
 volatile int tick = 0; //encoder ticks
+int e;//error
+int olde=0;//old error
+int E=0;//integrated error or cumulated error
+int de;//derived error or error variation
+const int kp=10;//proportional wheight
+const int ki=10;//integral wheight
+const int kd=1;//derivative wheight
+int PID;//result of pid calc before mapping to pins
+int mapped;//mapped version of pid
+const int maxpid=4000;//max value of pid for mapping
+const int minpid=0;//min valu of pid use for mapping
 
 //Communication
-const int bauderate = 9600;
-
+const int bauderate = 38400;
 
 //encoder
 const int encoderpin = 8;// pin for the encoder interrupt
 void encoderInterrupt();//isr for encoder
+
+// H-bridge
+const int pwmpin=10;//pin for H bridge pwm
+void motorbreak();
--- PIDTeensy/src/main.cpp
+++ PIDTeensy/src/main.cpp
@@ -1,37 +1,58 @@
-#include <TimerOne.h>
+#include "TimerOne.h"
 #include <Arduino.h>
 #include "header.h"
 
-const int led=LED_BUILTIN;//used for debug
 void setup(void)
 {
   //TIMER initialization
-  pinMode(led,OUTPUT); //used for debug
   Timer1.initialize(period); //initialisation du timer
   Timer1.attachInterrupt(Cycle); //attachInterrupt
   //encoder initialisation
   attachInterrupt(encoderpin,encoderInterrupt,RISING);
-  Serial.begin(bauderate);
-
+  //setting up pwm precision
+  analogWriteFrequency(pwmpin,F_CPU/1E6);//setting up ideal frequency pedending on cpu frequency
+  analogWriteResolution(10); // 0 - 255
+  //ros here pls
+  Serial.begin(bauderate);//comm non ros
 }
 
 void loop(void)
 {
-  delay(300);
-  Serial.println("a delay has ended");
-  delay(500);
-  Serial.println("other");
-  digitalWrite(led,LOW);
+  delay(1000);
+  Serial.println("------- 1 sec is over -------");
+  Serial.send_now();
 }
 
 void Cycle()
 {
   cli(); //éteint les interrupts
-  Serial.println(tick);
+  if(emergency_break){
+    motorbreak();
+  }else if(target_duration>0){
+    //calculate error and pid
+    e = target_ticks - tick;
+    E = E+e;
+    de = olde-e;
+    PID = (kp*e)+(ki*E)+(kd*de);
+    mapped = map(PID,minpid,maxpid,0,1023);
+    analogWrite(pwmpin,mapped);
+    //reset
+    olde = e;
+    tick = 0;
+    target_duration--;
+    //for debug
+    Serial.print(e);
+    Serial.print(" ");
+    Serial.println(PID);
+    Serial.send_now();
+  }
   sei(); //relance les interrupts
 }
 
 void encoderInterrupt(){
   tick++;
-  digitalWrite(led,HIGH);
+}
+
+void motorbreak(){
+
 }
