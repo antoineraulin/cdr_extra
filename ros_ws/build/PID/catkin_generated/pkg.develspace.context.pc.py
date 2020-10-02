755b4e8e7e21c983387d9cbdae94fb99f83da7a7
antoin.raulin@icloud.com <antoin.raulin@icloud.com>
2020-10-02 14:35:18 +0200

    fixed include error

--- ros_ws/build/PID/catkin_generated/pkg.develspace.context.pc.py
+++ ros_ws/build/PID/catkin_generated/pkg.develspace.context.pc.py
@@ -1,7 +1,7 @@
 # generated from catkin/cmake/template/pkg.context.pc.in
 CATKIN_PACKAGE_PREFIX = ""
 PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/antoine/cdr_extra/ros_ws/devel/include".split(';') if "/home/antoine/cdr_extra/ros_ws/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "message_runtime".replace(';', ' ')
+PROJECT_CATKIN_DEPENDS = "rospy;roscpp;message_runtime".replace(';', ' ')
 PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
 PROJECT_NAME = "PID"
 PROJECT_SPACE_DIR = "/home/antoine/cdr_extra/ros_ws/devel"
--- ros_ws/devel/share/PID/cmake/PIDConfig.cmake
+++ ros_ws/devel/share/PID/cmake/PIDConfig.cmake
@@ -185,7 +185,7 @@ foreach(t ${PID_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "message_runtime")
+set(depends "rospy;roscpp;message_runtime")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
